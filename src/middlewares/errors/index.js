afb591bed4c009204283570fa1aaa63249189fa8
Catalina Riquelme <riquelmecata@gmail.com>
2023-12-16 22:07:18 -0300

    add cases to switch

--- src/middlewares/errors/index.js
+++ src/middlewares/errors/index.js
@@ -1,12 +1,20 @@
 import EErrors from "../../services/errors/enum.js";
 
-export default (error,req,res,next)=>{
+
+export default (error, req, res, next) => {
     console.log(error.cause);
     switch (error.code) {
         case EErrors.INVALID_TYPES_ERROR:
-            res.send({status: "error", error:error.name})
+            res.send({ status: "error", error: error.name });
+            break;
+        case EErrors.ROUTING_ERROR:
+            res.status(404).send({ status: "error", error: "Routing error occurred" });
+            break;
+        case EErrors.DATABASE_ERROR:
+            res.status(500).send({ status: "error", error: "Database error occurred" });
             break;
         default:
-            res.send({status: "error", error: "Unhandled error"})
+            res.send({ status: "error", error: "Unhandled error" });
     }
-}
+};
+
